4df29b74dfc392c1f395b0bf2d29e9cf3937c370
rgtjf1@163.com <rgtjf1@163.com>
2016-03-10 20:14:44 +0800

    finished!

--- grid_search.py
+++ grid_search.py
@@ -1,42 +1,26 @@
 
 candidate = [ i/10.0 for i in range(6, 16, 2)]
 
-print candidate
-
-
 #n, v, a, r, #
-param_grid = [
+param_grids = [
  	{ 'n': candidate, 'v': candidate, 'a': candidate, 'r': candidate, '#': candidate }
  	#{'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
  ]
 
-
-def eval(param_dict):
-	#print param_dict
-	#print cnt
-	global cnt 
-	cnt = cnt + 1
-	pass
-	
-	VecDict
-	ConfigDict
-	Input 
-	Gs
-	Eval_func()
-
-
-def search(param_dict, candidata_dict):
-	if not candidata_dict:
-		eval(param_dict)
-	else:
-		key, value_list = candidata_dict.items()[0]
-		newcandidata_dict = candidata_dict.copy()
-		newcandidata_dict.pop(key)
-		for value in value_list:
-			param_dict[key] = value
-			search(param_dict, newcandidata_dict)
-	# 
-
-cnt = 0
-search({}, param_grid[0])
-print cnt
+def product(param_grid):
+	'''
+	test = {'n': [1,2], 'a': [2, 3]}
+	it = product(test)
+	try:
+		while True:
+			x = it.next()
+			print x
+	except StopIteration:
+		pass
+	'''
+	pools = param_grid.values()
+	result = [[]]
+	for pool in pools:
+		result = [x+[y] for x in result for y in pool]
+	for prod in result:
+		yield dict(zip(param_grid.keys(), prod))
